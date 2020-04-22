e9df0c89a7151835f412778fec77089a1b5471fc
Elton Marques <elton1314@hotmail.com>
2020-04-22 19:45:53 -0300

    feat(google analytics): adding Google Analytics Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
+require('dotenv').config()
+
 module.exports = {
-  plugins: [`gatsby-plugin-typescript`],
+  plugins: [
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.REACT_APP_GOOGLE_ANALYTICS_ID,
+      },
+    },
+  ],
 };
