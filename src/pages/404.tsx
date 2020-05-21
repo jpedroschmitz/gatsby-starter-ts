af48bb923ffe872bf98c15b1b1915cbe07f6a5d7
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2020-05-21 17:44:42 -0300

    chore: import and use PageProps

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import { PageProps } from 'gatsby';
 
-const NotFound: React.FC<{}> = () => {
-  return (
-    <main>
-      <p>Sorry, page not found!</p>
-    </main>
-  );
-};
+const NotFound: React.FC<PageProps> = () => (
+  <main>
+    <p>Sorry, page not found!</p>
+  </main>
+);
 
 export default NotFound;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
+import { PageProps } from 'gatsby';
 
 import Title from '@/components/Title';
 
-const Home: React.FC<{}> = () => {
-  return (
-    <main>
-      <Title />
-      <p>
-        This is a nice TypeScript starter for Gatsby, great for advanced users!
-      </p>
-    </main>
-  );
-};
+const Home: React.FC<PageProps> = () => (
+  <main>
+    <Title />
+    <p>
+      This is a nice TypeScript starter for Gatsby, great for advanced users!
+    </p>
+  </main>
+);
 
 export default Home;
