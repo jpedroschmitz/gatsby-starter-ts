95da2bd029a101e84483d67e5de4166c9bb83ca1
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2020-05-21 17:48:22 -0300

    chore: update gatsby and remove gatsby-plugin-typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  plugins: [`gatsby-plugin-typescript`],
+  // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
+  // don't need to define it here (just if you need to change the options)
+  plugins: [],
 };
