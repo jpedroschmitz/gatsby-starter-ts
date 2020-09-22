486fd9649f3ff0d6463c6c1936ab9c869b63975e
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2020-09-22 14:14:17 -0300

    chore: update all devDependencies and fix eslint errors

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const Title: React.FC<{}> = () => {
-  return <h1>Hello TypeScript!</h1>;
-};
+const Title: React.FC = () => <h1>Hello TypeScript!</h1>;
 
 export default Title;
