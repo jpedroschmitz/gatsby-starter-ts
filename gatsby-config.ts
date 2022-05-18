19018b2c14223ac0c486b239703931c6460d921d
Nick Spaargaren <nickspaargaren@protonmail.com>
2022-05-18 18:29:15 +0200

    refactor: migrate gatsby-config to TypeScript (#503)
    Co-authored-by: Nick Spaargaren <nickspaargaren@users.noreply.github.com>

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,9 @@
-module.exports = {
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
   plugins: [],
 };
+
+export default config;
