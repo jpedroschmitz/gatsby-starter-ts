1c2ba067740730307c04b359685ac0841ed69b72
João Pedro Schmitz <oi@joaopedro.cc>
2022-05-18 15:51:42 -0300

    chore: update jest config, .gitignore, and move files to TS (#505)

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,27 @@
 module.exports = {
-    cacheDirectory: '<rootDir>/.cache/unit',
-    globals: {
-        'ts-jest': {
-          tsconfig: 'tsconfig.spec.json',
-        },
-        __PATH_PREFIX__: ``,
+  preset: 'ts-jest',
+  rootDir: '.',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
     },
-    moduleDirectories: ['node_modules', 'src'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    moduleNameMapper: {
-      '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-      '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
-      '@/(.*)': '<rootDir>/src/$1',
-    },
-    preset: 'ts-jest',
-    rootDir: '.',
-    setupFiles: [`<rootDir>/__helpers__/loadershim.js`],
-    setupFilesAfterEnv: ['<rootDir>/__helpers__/setup.ts'],
-    testEnvironment: 'jsdom',
-    testPathIgnorePatterns: [
-      '<rootDir>/__mocks__/*',
-      'node_modules',
-      '\\.cache',
-      '<rootDir>.*/public',
-    ],
-    testURL: `http://localhost`,
-    transform: { '^.+\\.[tj]sx?$': 'ts-jest' },
-    transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
-  };
+    __PATH_PREFIX__: ``,
+    __BASE_PATH__: ``,
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.ts`,
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  setupFiles: [`<rootDir>/__helpers__/loadershim.ts`],
+  setupFilesAfterEnv: ['<rootDir>/__helpers__/setup.ts'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.svg$': '<rootDir>/__mocks__/svgTransform.ts',
+  },
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+};
