c5b70ba807b46755627c691ce9f342801d6544cd
Nick Spaargaren <nickspaargaren@protonmail.com>
2022-05-18 18:30:24 +0200

    refactor: migrate gatsby-node to TypeScript (#504)
    Co-authored-by: Nick Spaargaren <nickspaargaren@users.noreply.github.com>

--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      plugins: [new TsconfigPathsPlugin()],
-    },
-  });
-};
---
+++ gatsby-node.ts
@@ -0,0 +1,12 @@
+import type { GatsbyNode } from 'gatsby';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    resolve: {
+      plugins: [new TsconfigPathsPlugin()],
+    },
+  });
+};
