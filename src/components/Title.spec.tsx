14e7258eb1b900232cff80efdebb30aae21c6d08
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2022-05-18 15:16:57 -0300

    chore: general updates

--- src/components/Title.spec.tsx
+++ src/components/Title.spec.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-
 import { render, screen } from '@testing-library/react';
 
-import Title from './Title';
+import Title from '@/components/Title';
 
 describe(`Title`, () => {
   it(`renders a Title component`, () => {
