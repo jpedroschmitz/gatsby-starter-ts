236f0aa0481ffef6036589924e4599ed9e58c62b
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2022-05-22 11:10:01 -0300

    chore: try to fix ci

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ const config: GatsbyConfig = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
   plugins: [],
-  jsxRuntime: 'automatic',
+  jsxRuntime: `automatic`,
 };
 
 export default config;
